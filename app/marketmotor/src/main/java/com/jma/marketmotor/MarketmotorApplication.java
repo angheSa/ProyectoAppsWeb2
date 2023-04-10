ddc3deed5ecbf4fab95c342c9f6fe6e2c7d7f28e
Jose <jose@MacBook-Air-de-Jose.local>
2023-04-10 06:34:27 -0500

    blank project added

---
+++ app/marketmotor/src/main/java/com/jma/marketmotor/MarketmotorApplication.java
@@ -0,0 +1,13 @@
+package com.jma.marketmotor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketmotorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketmotorApplication.class, args);
+    }
+
+}
---
+++ app/marketmotor/src/test/java/com/jma/marketmotor/MarketmotorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jma.marketmotor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketmotorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
